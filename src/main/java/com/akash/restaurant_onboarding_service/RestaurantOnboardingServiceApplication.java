c7641a5703cdf0f078cddb0440181f0c528b71ec
Akash <akash.bhuiyan@gmail.com>
2025-11-14 01:41:33 +0100

    project added

---
+++ src/main/java/com/akash/restaurant_onboarding_service/RestaurantOnboardingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.akash.restaurant_onboarding_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOnboardingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantOnboardingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akash/restaurant_onboarding_service/RestaurantOnboardingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.restaurant_onboarding_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOnboardingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
